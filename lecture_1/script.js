4b34a4b754323df2b086675f62fec02213977f4b
RybiRoid <legenservis@tut.by>
2021-12-01 01:51:20 +0300

    first commit

---
+++ lecture_1/script.js
@@ -0,0 +1,91 @@
+var message = 'Random text';
+var count = 3;
+var testCount = 5;
+var isLoading = false;
+var text = "Result\ntest";
+
+//console.log( -count );
+// console.log( count++ );
+// console.log(count);
+
+//++testCount;
+//testCount = testCount + 1;
+testCount /= 10;  // -= *= /=
+//console.log(testCount)
+//console.log(9 >= 9);
+
+// "=="
+//console.log('helloh74' == "hello"); //нестрогое равенство 
+//console.log('hello' != "hello");
+
+
+// "===" строгое равенство
+//console.log(10 != '10');
+
+//var result = false;
+//console.log(!result);
+
+
+var age = 18;
+
+// if(age > 21){
+//     console.log('Вам меньше 21');
+// } else {
+//     console.log('!!!!!!!!!!!!!!!!!!!');
+// }
+
+//switch case
+
+/* switch(19){
+    case 10:
+        console.log('Число 10');
+    break;
+    case 11:
+        console.log('Число 11');
+        break;
+    case 0:
+        console.log('0000000000');
+        break;
+    default:
+        console.log('default message!');
+} */
+
+
+//console.log('hello world!', 100, age);
+//alert('Hello World!!!!!!');
+//prompt('Сколько времени?', '21:04');
+//confirm('yes or no?');
+//document.write('test str', 1000);
+
+
+/* console.log(5);
+console.log(4);
+console.log(3);
+console.log(2);
+console.log(1); */
+
+var currentCount = 5;
+
+/* while(currentCount > 0){
+    console.log('test message', currentCount);
+    currentCount--;
+} */
+
+/* do{
+    console.log('test do while', currentCount);
+    currentCount--;
+    
+}while(currentCount > 5); */
+
+
+for(var i = 5; i > 0; i--){
+    console.log('message for:', i);
+}
+
+for( var i=0;i < 5; i++);{
+    console.log(i);
+}
+
+
+
+
