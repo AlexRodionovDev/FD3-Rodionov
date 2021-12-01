78ddec9d334ecd44b244bdaee7d0e8003a5f99c6
RybiRoid <legenservis@tut.by>
2021-12-01 17:44:07 +0300

    3 commit

---
+++ React-HW1/ishop/react.js
@@ -0,0 +1,7 @@
+const products =[
+    {name: 'tv', price: 700, photo: 'https://www.millionpodarkov.ru/incoming_img/pleer.ru/9203547.jpg', inStock: 100},
+    {name: 'smartphone', price: 300, photo: 'https://microless.com/cdn/products/d98a338fc1ea731d545043ca8d89a174-hi.jpg', inStock: 200},
+    {name: 'laptop', price: 1000, photo: 'https://ae01.alicdn.com/kf/Uf0c0a7b0ba0d46e9802508593e35ba85w/255-G7-39-6-15-6-1366x768-AMD-Ryzen-3-8.jpg', inStock: 50},
+    {name: 'fan', price: 70, photo: 'https://www.777555.by/content/thumb/items/img_2006524_big_1_1024.jpg', inStock: 100},
+    {name: 'mixer', price: 100, photo: 'https://imarket.by/upload/simpleimage_cache/aa00b6e87209b7c2a5f74d7cfaa4662e_1200x800_w0_100.jpg', inStock: 150},
+]
